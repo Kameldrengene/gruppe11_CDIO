7bda1da55d035af0eb32a5e48cc1af73c3ddd0a8
Volkan <volkanisik@live.dk>
2021-02-17 10:52:55 +0100

    popup1button.xml

--- app/src/main/java/com/example/gruppe11_cdio/GameActivity.java
+++ app/src/main/java/com/example/gruppe11_cdio/GameActivity.java
@@ -2,39 +2,16 @@ package com.example.gruppe11_cdio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button spil,highscore;
+public class GameActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
-        spil = findViewById(R.id.spil_button);
-        highscore = findViewById(R.id.hightscore_button);
-
-        spil.setOnClickListener(this);
-        highscore.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v==spil){
-            Intent intent = new Intent(this,GameActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
-        if(v==highscore){
-            Intent intent = new Intent(this,HighScoreActivity.class);
-            startActivity(intent);
-            finish();
-        }
     }
 }
--- app/src/main/java/com/example/gruppe11_cdio/MainActivity.java
+++ app/src/main/java/com/example/gruppe11_cdio/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.gruppe11_cdio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button spil,alertButton;
+    Dialog alertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spil = findViewById(R.id.spil_button);
+
+
+        spil.setOnClickListener(this);
+
+
+       View alertView = getLayoutInflater().inflate(R.layout.popup1button,null);
+
+        alertDialog = new Dialog(this);
+        TextView alertTitle = alertView.findViewById(R.id.alertTitle);
+        TextView alertBody = alertView.findViewById(R.id.alertBody);
+        alertButton = alertView.findViewById(R.id.alertButton);
+        alertButton.setOnClickListener(this);
+
+        alertTitle.setText("Velkommen til 7-kabale");
+        alertBody.setText("" +
+                "Denne app bruges til at hjælpe\n" +
+                "med 7-kabale.\n" +
+                "Du kan starte appen i hvilken\n" +
+                "som helst tidspunkt i et spil.\n" +
+                "Du tager så et billede ved at\n" +
+                "trykke på knappen “Tag billede”\n" +
+                "og appen lærer din situation at\n" +
+                "kende.\n" +
+                "Herefter, tager du et nyt billede\n" +
+                "efter hvert træk du tager.\n" +
+                "Hvis du vælger at stoppe med at\n" +
+                "bruge hjælpen, skal du gå ud af\n" +
+                "spillet ved at trykke på\n" +
+                "tilbage-pilen. Kabalen kan\n" +
+                "startes igen, hvornår som helst.");
+
+        alertDialog.setContentView(alertView);
+        alertDialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==spil){
+            Intent intent = new Intent(this,GameActivity.class);
+            startActivity(intent);
+        }
+
+        if(v==alertButton){
+            alertDialog.dismiss();
+        }
     }
 }
