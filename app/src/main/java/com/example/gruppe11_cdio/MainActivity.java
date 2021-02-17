379a9bf6be91daea8fc916021885cb769a66ea8b
Talha-Butt786 <s195475@student.dtu.dk>
2021-02-17 10:27:44 +0100

    Open cv configured with project

--- app/src/main/java/com/example/gruppe11_cdio/MainActivity.java
+++ app/src/main/java/com/example/gruppe11_cdio/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gruppe11_cdio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug()){
+            Toast.makeText(this,"open cv installed",Toast.LENGTH_SHORT);
+            System.out.println("open cv installed");
+        }else
+        {
+            Toast.makeText(this,"open cv not installed",Toast.LENGTH_SHORT);
+            System.out.println("open cv not installed");
+        }
     }
 }
